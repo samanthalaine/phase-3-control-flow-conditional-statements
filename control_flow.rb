d4d5c5f737efe6c0798b740e0d46ab9da54d6603
Samantha Laine <samanthalaine8@gmail.com>
2021-08-30 18:14:04 -0400

    commit tree ee4915a11af969b5f758d969a9900ff1c80b8f25

--- control_flow.rb
+++ control_flow.rb
@@ -1,16 +1,46 @@
+require "pry" 
+
 def admin_login(username, password)
-  # your code here
+
+  # binding.pry
+  if (username == "admin" || username == "ADMIN") && password == "12345" 
+    "Access granted"
+  else 
+    "Access denied"
+  end
 end
 
 def hows_the_weather(temperature)
-  # your code here
+  if temperature < 40
+    "It's brisk out there!"
+  elsif temperature >= 40 && temperature <= 65
+    "It's a little chilly out there!"
+    elsif 
+      temperature > 85 
+      "It's too dang hot out there!"
+      else 
+        "It's perfect out there!"
+      end
 end
 
 def fizzbuzz(num)
-  # your code here
+  case
+  when num % 5 == 0 && num % 3 == 0 then "FizzBuzz"
+  when num % 3  == 0 then "Fizz"
+  when num % 5  == 0 then "Buzz"
+  else num
+  end
 end
 
 def calculator(operation, num1, num2)
-  # your code here
+  case operation
+  when  '+' then num1 + num2
+  when '-' then num1 - num2
+  when  '*' then num1 * num2
+  when  '/' then num1 / num2
+  else 
+    puts "Invalid operation!"
+    nil
+  end
 end
 
